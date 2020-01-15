b3a57a084a703096de8fb37cb539c37ab7cfd74a
Rick Gaiser <rgaiser@gmail.com>
2020-01-15 21:59:18 +0100

    Use linkfile from ps2dev

--- ld/emulparams/elf32l5900.sh
+++ ld/emulparams/elf32l5900.sh
@@ -1,31 +1,4 @@
-SCRIPT_NAME=elf
-OUTPUT_FORMAT="elf32-littlemips"
-BIG_OUTPUT_FORMAT="elf32-bigmips"
-LITTLE_OUTPUT_FORMAT="elf32-littlemips"
-TEXT_START_ADDR=0x100000
-MAXPAGESIZE=128
-OTHER_TEXT_SECTIONS='*(.mips16.fn.*) *(.mips16.call.*)'
-OTHER_GOT_SYMBOLS='
-  _gp = ALIGN(16) + 0x7ff0;
-'
-OTHER_GOT_SECTIONS='
-  .lit8 : { *(.lit8) }
-  .lit4 : { *(.lit4) }
-'
-TEXT_START_SYMBOLS='_ftext = . ;'
-DATA_START_SYMBOLS='_fdata = . ;'
-OTHER_BSS_SYMBOLS='_fbss = .;'
-EXECUTABLE_SYMBOLS='_DYNAMIC_LINK = 0;'
-OTHER_SECTIONS='
-  .gptab.sdata : { *(.gptab.data) *(.gptab.sdata) }
-  .gptab.sbss : { *(.gptab.bss) *(.gptab.sbss) }
-  PROVIDE(_heap_size = -1);
-  PROVIDE(_stack = -1);
-  PROVIDE(_stack_size = 128 * 1024);
-'
 ARCH="mips:5900"
-MACHINE=
-TEMPLATE_NAME=elf32
-GENERATE_SHLIB_SCRIPT=yes
-DYNAMIC_LINK=FALSE
-EMBEDDED=yes
+OUTPUT_FORMAT="elf32-littlemips"
+SCRIPT_NAME=ps2dev
+TEMPLATE_NAME=generic
